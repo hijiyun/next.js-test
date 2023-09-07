1da954b1f2ac2a717135bc02f8583c7e502c1c30
박지윤 <tkrhkfld123@naver.com>
2023-09-07 23:33:08 +0900

    📝 Header module css 꾸미기

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+
 export default function Header():any{
     const router = useRouter();
     console.log("router???",router)
-    return <div>
+    return <div className="nav">
         <Link href="/" style={{color: router.pathname === '/'?'red':'blue'}}>Home</Link> <br></br>
         <Link href="about" style={{color: router.pathname === '/about'?'red':'blue'}}>About</Link>
     </div>
